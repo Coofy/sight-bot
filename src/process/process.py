d6cf04142cd6326b3d6c6ef2816636d1e21ef375
igor <igor.atf@gmail.com>
2019-10-30 21:18:32 -0300

    add person detection code

--- src/process/process.py
+++ src/process/process.py
@@ -1,35 +1,91 @@
+# Code adapted from Tensorflow Object Detection Framework
+# More on
+# https://github.com/tensorflow/models/blob/master/research/object_detection/object_detection_tutorial.ipynb
+# Tensorflow Object Detection Detector
+
 import numpy as np
+import tensorflow as tf
 import cv2
+import time
 
-UPPER_GREEN = [154,169,63] # brightest
-LOWER_GREEN = [7,6,1] # darkest
 
-class Process(object):
-    def __init__(self):
-        self.frames = []
+class DetectorAPI:
+    def __init__(self, path_to_ckpt):
+        self.path_to_ckpt = path_to_ckpt
 
-    def main(self):
-        # Code below is suitable for capturing video frames
-        # cap = cv2.VideoCapture('url')
-        # while True:
-        #   res, frame = cap.read()
-        #   if not res:
-        #       break
-        # Use frame to manipule frames
-        upper_green = np.array([UPPER_GREEN])
-        lower_green = np.array([LOWER_GREEN])
-        img = cv2.imread('./assets/cs.jpg')
+        self.detection_graph = tf.Graph()
+        with self.detection_graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(self.path_to_ckpt, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
 
-        # Threshold the RGB image to get only green colors
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        mask = cv2.inRange(img, lower_green, upper_green)
+        self.default_graph = self.detection_graph.as_default()
+        self.sess = tf.Session(graph=self.detection_graph)
 
-        cv2.imshow('image', img)
-        cv2.imshow('mask', mask)
-        k = cv2.waitKey(0)
-        if k == 27:
-            return
+        # Definite input and output Tensors for detection_graph
+        self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+        # Each box represents a part of the image where a particular object was detected.
+        self.detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+        # Each score represent how level of confidence for each of the objects.
+        # Score is shown on the result image, together with the class label.
+        self.detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+        self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+        self.num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
+
+    def processFrame(self, image):
+        # Expand dimensions since the trained_model expects images to have shape: [1, None, None, 3]
+        image_np_expanded = np.expand_dims(image, axis=0)
+        # Actual detection.
+        start_time = time.time()
+        (boxes, scores, classes, num) = self.sess.run(
+            [self.detection_boxes, self.detection_scores, self.detection_classes, self.num_detections],
+            feed_dict={self.image_tensor: image_np_expanded})
+        end_time = time.time()
+
+        print("Elapsed Time:", end_time-start_time)
+
+        im_height, im_width,_ = image.shape
+        boxes_list = [None for i in range(boxes.shape[1])]
+        for i in range(boxes.shape[1]):
+            boxes_list[i] = (int(boxes[0,i,0] * im_height),
+                        int(boxes[0,i,1]*im_width),
+                        int(boxes[0,i,2] * im_height),
+                        int(boxes[0,i,3]*im_width))
+
+        return boxes_list, scores[0].tolist(), [int(x) for x in classes[0].tolist()], int(num[0])
+
+    def close(self):
+        self.sess.close()
+        self.default_graph.close()
 
 if __name__ == "__main__":
-    PROCESS = Process()
-    PROCESS.main()
+    model_path = './faster_rcnn_inception_v2_coco_2018_01_28/frozen_inference_graph.pb'
+    odapi = DetectorAPI(path_to_ckpt=model_path)
+    threshold = 0.7
+    cap = cv2.VideoCapture('./cs-video-cut.mp4')
+
+    while True:
+        r, img = cap.read()
+        # img = cv2.resize(img, (1280, 720))
+
+        boxes, scores, classes, num = odapi.processFrame(img)
+
+        # Visualization of the results of a detection.
+
+        for i in range(len(boxes)):
+            # Class 1 represents human
+            if classes[i] == 1 and scores[i] > threshold:
+                box = boxes[i]
+                cv2.rectangle(img,(box[1],box[0]),(box[3],box[2]),(255,0,0),2)
+
+        cv2.imshow("preview", img)
+        key = cv2.waitKey(1)
+        if key & 0xFF == ord('q'):
+            break
+
+
+
+## Note: running extremely low, let's find out a way to improve it
+#  and we extract the useful detections later
